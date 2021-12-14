35b3bac15a1b07b6b99c47150e93dc6b0904bb45
unknown <hot99009@gmail.com>
2021-12-14 14:43:00 +0900

    test

---
+++ Sever/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Sever
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbServerIP = new System.Windows.Forms.TextBox();
+            this.tbSeverPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tbSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(18, 19);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(994, 394);
+            this.tbConsole.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1043, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(361, 373);
+            this.textBox2.TabIndex = 1;
+            // 
+            // tbServerIP
+            // 
+            this.tbServerIP.Location = new System.Drawing.Point(18, 419);
+            this.tbServerIP.Multiline = true;
+            this.tbServerIP.Name = "tbServerIP";
+            this.tbServerIP.Size = new System.Drawing.Size(215, 68);
+            this.tbServerIP.TabIndex = 2;
+            // 
+            // tbSeverPort
+            // 
+            this.tbSeverPort.Location = new System.Drawing.Point(18, 499);
+            this.tbSeverPort.Name = "tbSeverPort";
+            this.tbSeverPort.Size = new System.Drawing.Size(215, 31);
+            this.tbSeverPort.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 68);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Listening";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Find Ip";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1043, 419);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(361, 31);
+            this.textBox5.TabIndex = 6;
+            // 
+            // tbSend
+            // 
+            this.tbSend.Location = new System.Drawing.Point(1043, 456);
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(361, 31);
+            this.tbSend.TabIndex = 7;
+            this.tbSend.Text = "Send";
+            this.tbSend.UseVisualStyleBackColor = true;
+            this.tbSend.Click += new System.EventHandler(this.tbSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1043, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "clients";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1416, 547);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSeverPort);
+            this.Controls.Add(this.tbServerIP);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbConsole);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbServerIP;
+        private System.Windows.Forms.TextBox tbSeverPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button tbSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1_Elasped;
+    }
+}
+
