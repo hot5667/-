e35078fdf9bb983544cb31ce950f45c4e563beee
KimDoHyun1011 <hot99009@gmail.com>
2021-12-14 16:47:43 +0900

    초기 커밋.

---
+++ TCPClient01/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TCPClient01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.tbPayload = new System.Windows.Forms.TextBox();
+            this.tbServerPort = new System.Windows.Forms.TextBox();
+            this.tbServerIP = new System.Windows.Forms.TextBox();
+            this.tbSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(7, 11);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(759, 285);
+            this.tbConsole.TabIndex = 0;
+            // 
+            // tbPayload
+            // 
+            this.tbPayload.Location = new System.Drawing.Point(516, 303);
+            this.tbPayload.Name = "tbPayload";
+            this.tbPayload.Size = new System.Drawing.Size(250, 21);
+            this.tbPayload.TabIndex = 1;
+            this.tbPayload.Text = "TcpClient01";
+            // 
+            // tbServerPort
+            // 
+            this.tbServerPort.Location = new System.Drawing.Point(7, 330);
+            this.tbServerPort.Name = "tbServerPort";
+            this.tbServerPort.Size = new System.Drawing.Size(250, 21);
+            this.tbServerPort.TabIndex = 2;
+            this.tbServerPort.Text = "5025";
+            // 
+            // tbServerIP
+            // 
+            this.tbServerIP.Location = new System.Drawing.Point(7, 303);
+            this.tbServerIP.Name = "tbServerIP";
+            this.tbServerIP.Size = new System.Drawing.Size(250, 21);
+            this.tbServerIP.TabIndex = 3;
+            this.tbServerIP.Text = "127.0.0.1";
+            // 
+            // tbSend
+            // 
+            this.tbSend.Location = new System.Drawing.Point(516, 330);
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(250, 21);
+            this.tbSend.TabIndex = 4;
+            this.tbSend.Text = "&Send";
+            this.tbSend.UseVisualStyleBackColor = true;
+            this.tbSend.Click += new System.EventHandler(this.tbSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(7, 357);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(250, 21);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 387);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.tbServerIP);
+            this.Controls.Add(this.tbServerPort);
+            this.Controls.Add(this.tbPayload);
+            this.Controls.Add(this.tbConsole);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "TCPClient 01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.TextBox tbPayload;
+        private System.Windows.Forms.TextBox tbServerPort;
+        private System.Windows.Forms.TextBox tbServerIP;
+        private System.Windows.Forms.Button tbSend;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ TCPClient01/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCPClient01
+{
+    public partial class Form1 : Form
+    {
+        TcpClient mTcpClient;
+        byte[] mRx;
+
+        //서버에 연결되어 있는지 확인
+        bool bConnected = false;
+        private bool Connected { get => mTcpClient == null ? false : mTcpClient.Connected && bConnected; }
+
+        //접속시 사용한 IP PORT
+        int iPort;
+        IPAddress iPAddress;
+
+        //재접속 타이머
+        System.Timers.Timer timer1 = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 1000;
+            timer1.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Elasped);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (mTcpClient != null && Connected /*mTcpClient.Connected*/) return ; //연결이 되어 있는데 또 연결 방지용, 서버쪽에 연결된 노드가 늘어남
+
+            IPAddress ipa;
+            int nPort;
+
+            try
+            {
+                if (string.IsNullOrEmpty(tbServerIP.Text) || string.IsNullOrEmpty(tbServerPort.Text))
+                    return;
+
+                if (!IPAddress.TryParse(tbServerIP.Text, out ipa))
+                {
+                    MessageBox.Show("Please supply an IP Address.");
+                    return;
+                }
+
+                if (!int.TryParse(tbServerPort.Text, out nPort))
+                {
+                    nPort = 23000;
+                }
+
+                mTcpClient = new TcpClient();
+                mTcpClient.BeginConnect(ipa, nPort, onCompleteConnect, mTcpClient);
+
+                iPAddress   = ipa  ;
+                iPort       = nPort;
+                bConnecting = true ;
+                timer1.Start();
+
+
+            }
+            catch (Exception exc)
+            {
+                bConnected = false;
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        bool bFirstConnect = false;
+        bool bConnecting = false ;
+        void onCompleteConnect(IAsyncResult iar)
+        {
+            TcpClient tcpc;
+
+            try
+            {
+                //iar.AsyncWaitHandle.WaitOne(1000, false);
+
+                tcpc = (TcpClient)iar.AsyncState;
+                tcpc.EndConnect(iar);
+                mRx = new byte[512];
+                tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromServerStream, tcpc);
+                bConnected = true; //연결 성공
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                bConnected = false; //연결 실패
+
+            }
+            bConnecting = false;
+        }
+
+        void onCompleteReadFromServerStream(IAsyncResult iar)
+        {
+            TcpClient tcpc;
+            int nCountBytesReceivedFromServer;
+            string strReceived;
+            
+            try
+            {
+                tcpc = (TcpClient)iar.AsyncState;
+                nCountBytesReceivedFromServer = tcpc.GetStream().EndRead(iar);
+
+                if (nCountBytesReceivedFromServer == 0)
+                {
+                    MessageBox.Show("Connection broken.");
+                    return;
+                }
+                strReceived = Encoding.ASCII.GetString(mRx, 0, nCountBytesReceivedFromServer);
+                string s1 = Encoding.ASCII.GetString(mRx, 0, nCountBytesReceivedFromServer-1);
+                if(mRx[nCountBytesReceivedFromServer-1] != '\n') return;
+                printLine(strReceived);
+
+                mRx = new byte[512];
+                tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromServerStream, tcpc);
+
+                bConnected = true; //연결 성공
+
+            }
+            catch (Exception exc)
+            {
+
+                bConnected = false; //연결 실패
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void printLine(string _strPrint)
+        {
+            tbConsole.Invoke(new Action<string>(doInvoke), _strPrint);
+        }
+
+        public void doInvoke(string _strPrint)
+        {
+            tbConsole.Text = _strPrint + Environment.NewLine + tbConsole.Text;
+        }
+
+        byte[] tx;
+        private void tbSend_Click(object sender, EventArgs e)
+        {
+            //byte[] tx;
+            
+            if (string.IsNullOrEmpty(tbPayload.Text)) return;
+
+            try
+            {
+                if (!Connected) return;
+
+                tx = Encoding.ASCII.GetBytes(tbPayload.Text + "\r\n");
+
+                if (mTcpClient != null)
+                {
+                    if (mTcpClient.Client.Connected)
+                    {
+                        mTcpClient.GetStream().BeginWrite(tx, 0, tx.Length, onCompleteWriteToServer, mTcpClient);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                bConnected = false;
+                MessageBox.Show(exc.Message);
+            }
+        }
+        void onCompleteWriteToServer(IAsyncResult iar)
+        {
+            TcpClient tcpc;
+
+            try
+            {
+                tcpc = (TcpClient)iar.AsyncState;
+                tcpc.GetStream().EndWrite(iar);
+                //bCompleted = true;
+            }
+            catch (Exception exc)
+            {
+                //bCompleted = false;
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void timer1_Elasped(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            bConnected = mTcpClient.Connected;
+            if (!mTcpClient.Connected && !bConnecting)
+            {
+                bConnecting = true;
+                mTcpClient.Close();
+                mTcpClient = new TcpClient();
+                mTcpClient.BeginConnect(iPAddress, iPort, onCompleteConnect, mTcpClient);
+            }
+            timer1.Enabled = true;
+        }
+
+
+    }
+}
